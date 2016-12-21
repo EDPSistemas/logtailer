11715f94fae30312ce78b3a16e28e8ee03a8f9ac
Ciro Souza <ciro.souza@grupofolha.com.br>
2016-12-21 19:06:42 -0200

    Maluquices de meu deus

--- logtailer.php
+++ logtailer.php
@@ -2,8 +2,19 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-use Symfony\Component\Console\Application;
+use Dotenv\Dotenv;
 use EDP\LogTailer\Commands\Tail as TailCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Yaml\Yaml;
+
+
+
+$dotenv = new Dotenv(__DIR__);
+$dotenv->load();
+
+$value = Yaml::parse(file_get_contents(getenv('YAML_CONFIG_FILE')));
+
+print_r($value);
 
 $application = new Application();
 $application->add(new TailCommand());
